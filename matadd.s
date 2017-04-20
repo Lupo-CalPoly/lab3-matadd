17d1292629e3687604146b4330c1778b12a86133
Chris Lupo <clupo@calpoly.edu>
2017-04-20 06:51:12 -0700

    Updated for integers

--- matadd.s
+++ matadd.s
@@ -1,14 +1,7 @@
-	.arch armv7-a
-	.text
-	.align	2
-	.global	matadd
-	.syntax unified
-	.arm
-matadd:
     /* This function has 5 parameters, and the declaration in the
        C-language would look like:
 
-       void matadd (float **C, float **A, float **B, int height, int width)
+       void matadd (int **C, int **A, int **B, int height, int width)
 
        C, A, B, and height will be passed in r0-r3, respectively, and
        width will be passed on the stack.
@@ -20,4 +13,18 @@ matadd:
 
        You should start with two for-loops that iterate over the height and
        width of the matrices, load each element from A and B, compute the
-       sum, then store the result to the correct element of C. */
+       sum, then store the result to the correct element of C. 
+
+       This function will be called multiple times by the driver, 
+       so don't modify matrices A or B in your implementation.
+
+       As usual, your function must obey correct ARM register usage
+       and calling conventions. */
+
+	.arch armv7-a
+	.text
+	.align	2
+	.global	matadd
+	.syntax unified
+	.arm
+matadd:
